6077dc93f011451a17f717aa47769001e7f570c7
Ian Mora <ianmorar03@gmail.com>
2021-09-28 12:15:50 -0600

    Adding Chalk and wipe script

--- public/db/wipe.js
+++ public/db/wipe.js
@@ -1,4 +1,5 @@
 var myArgs = process.argv.slice(2);
+const chalk = require('chalk');
 var sqlite3 = require("sqlite3").verbose();
 
 let db = new sqlite3.Database('./public/db/database.db', (err) => {
@@ -8,103 +9,140 @@ let db = new sqlite3.Database('./public/db/database.db', (err) => {
         }
         return;
     }else{
-        console.log('Connected to SQlite database.');
+        console.log('Connected to SQlite database\n');
         checkOptions(myArgs);
     }
 });
 
 function checkOptions(params) {
-    if(params.length == 0){
-        console.log("No parameters given");
-        return;
-    }
-    if(params[0] == "all"){
-        console.log("Wiping all data");
-        wipeAll();
-    }else if(params[0] == "tickets"){
-        //wipeTickets();
+    if(params.length != 0){
+        switch (params[0]) {
+            case "all":
+                wipeAll();
+                break;
+            case "tickets":
+                //wipeTickets();
+                break;
+            default:
+                console.log(chalk.red('[ERR]'),"Invalid parameter");
+                break;
+        }
     }else{
-        console.log("Invalid parameter");
-        return;
+        console.log(chalk.red('[ERR]'),"No options specified");
     }
+    return;
 }
-async function wipeAll(){
+var intervals = new Array();
+function wipeAll(){
     dropTables().then(()=>{
+        console.log(chalk.green('[OK]'),"TABLES DROPPED\n")
         createTables().then(()=>{
-            console.log('\nTask executed succesfully')
+            intervals.forEach(interval => {
+                clearInterval(interval);
+            });
+            console.log(chalk.green('[OK]'),'TABLES CREATED\n');
+            console.log(chalk.green('\nTask executed succesfully'))
+            return;
         })
     })
-    
 }
 async function dropTables(){
-    db.serialize(()=>{
-        db.run("DROP TABLE ticket_d", (err) => {
-            if(err){
-                console.log("Error dropping tickets");
-                return;
-            }
-            console.log("Tickets with a deploy related wiped");
-        })
-        .run("DROP TABLE ticket_l", (err) => {
-            if(err){
-                console.log("Error dropping tickets");
-                return;
-            }
-            console.log("Tickets n-r dropped");
-        })
-        .run("DROP TABLE deployment", (err) => {
-            if(err){
-                console.log("Error dropping deployments");
-                return;
-            }
-            console.log("Deployments dropped");
+    return new Promise((resolve, reject)=>{
+        console.log(chalk.bold.underline("Dropping tables..."));
+        var tables = 0;
+        db.serialize(()=>{
+            db.run("DROP TABLE ticket_d", (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error dropping ticket_d");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('-'),"ticket_d dropped");
+            })
+            .run("DROP TABLE ticket_l", (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error dropping ticket_l");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('-'),"ticket_l dropped");
+            })
+            .run("DROP TABLE deployment", (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error dropping deployment");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('-'),"deployment dropped");
+            });
+            intervals.push(setInterval(() => {
+                if(tables == 3){
+                    resolve();
+                }
+            }, 500));
         });
-    });
-    console.log("Tables dropped")
+    })
 }
 async function createTables(){
-    db.serialize(()=>{
-        var deployment_sql = `CREATE TABLE deployment (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text NOT NULL,
-            model text
-        )`;
-        var ticket_d_sql = `CREATE TABLE ticket_d (
-            id INTEGER,
-            deploy INTEGER NOT NULL,
-            code text NOT NULL UNIQUE,
-            priority TEXT,
-            release text,
-            FOREIGN KEY("deploy") REFERENCES "deployment"("id") ON DELETE CASCADE ON UPDATE NO ACTION,
-            PRIMARY KEY("id" AUTOINCREMENT)
-        )`;
-        var ticket_l_sql = `CREATE TABLE ticket_l (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            code text NOT NULL UNIQUE,
-            priority text NOT NULL,
-            release text NOT NULL
-        )`;
-        db.run(deployment_sql, (err) => {
-            if(err){
-                console.log("Error creating deployments");
-                return;
-            }
-            console.log("Deployments created");
-        })
-        .run(ticket_d_sql, (err) => {
-            if(err){
-                console.log("Error creating tickets");
-                return;
-            }
-            console.log("Tickets created");
-        })
-        .run(ticket_l_sql, (err) => {
-            if(err){
-                console.log("Error creating tickets");
-                return;
-            }
-            console.log("Tickets created");
-        })
-        console.log('TABLES CREATED');
-    });
+    return new Promise((resolve, reject)=>{
+        console.log(chalk.bold.underline("Creating tables..."));
+        var tables = 0;
+        db.serialize(()=>{
+            var deployment_sql = `CREATE TABLE deployment (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name text NOT NULL,
+                model text
+            )`;
+            var ticket_d_sql = `CREATE TABLE ticket_d (
+                id INTEGER,
+                deploy INTEGER NOT NULL,
+                code text NOT NULL UNIQUE,
+                priority TEXT,
+                release text,
+                FOREIGN KEY("deploy") REFERENCES "deployment"("id") ON DELETE CASCADE ON UPDATE NO ACTION,
+                PRIMARY KEY("id" AUTOINCREMENT)
+            )`;
+            var ticket_l_sql = `CREATE TABLE ticket_l (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                code text NOT NULL UNIQUE,
+                priority text NOT NULL,
+                release text NOT NULL
+            )`;
+            db.run(deployment_sql, (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error creating deployment");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('+'),"deployment created");
+            })
+            .run(ticket_d_sql, (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error creating ticket_d");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('+'),"ticket_d created");
+            })
+            .run(ticket_l_sql, (err) => {
+                if(err){
+                    console.log(chalk.red('[ERR]'),"Error creating ticket_l");
+                    reject(err);
+                    return;
+                }
+                tables++;
+                console.log(chalk.bgMagenta.bold('+'),"ticket_l created");
+            })
+            intervals.push(setInterval(() => {
+                if(tables == 3){
+                    resolve();
+                }
+            }, 500));
+        });
+    })
 }
