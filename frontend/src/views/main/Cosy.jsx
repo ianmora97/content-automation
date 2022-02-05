ffa1ac83163941a1a386e28fca98deb779967e8a
Ian Mora <ianmorar03@gmail.com>
2022-02-05 14:34:27 -0600

    select NA code

--- frontend/src/views/main/Cosy.jsx
+++ frontend/src/views/main/Cosy.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 export class Cosy extends Component {
     state = {
         naCodes: [],
+        naCodesShow: [],
+        currentSelectedNaCode: null
     }
     componentDidMount(){
         this.loadNACodes();
@@ -15,6 +17,7 @@ export class Cosy extends Component {
             if (res.status === 200) {
                 this.setState({
                     naCodes: res.data.data,
+                    naCodesShow: res.data.data
                 })
             } else {
                 console.log('Error: ', res.status);
@@ -23,24 +26,40 @@ export class Cosy extends Component {
             console.log(error);
         }
     }
+    selectNACode = (e) => {
+        const naCode = e.target.dataset.na;
+        this.setState({
+            currentSelectedNaCode: naCode,
+            naCodesShow: this.state.naCodes.filter(elem => elem.code === naCode)
+        })
+    }
+    closeNACODEsearch = (e) => {
+        console.log(e.target.checked);
+        e.target.checked = false;
+        this.setState({
+            currentSelectedNaCode: null,
+            naCodesShow: this.state.naCodes
+        })
+    }
     render() {
         return (
             <div style={{overflowX:"auto"}}>
-                <div className="bg-dark rounded-15 w-100 my-2" style={{height: "180px", overflow: "auto"}}>
+                <div className="bg-dark rounded-15 w-100 my-2" style={{height: "180px"}}>
                     <div className="px-3">
                         <label htmlFor="searchBaronModelCodes" className="my-2">Search model</label>
                         <input className="form-control bg-dark-light" style={{width: "400px"}} name="searchCosyNaCode" type="text" placeholder="Search" aria-label="Search Tab"/>
                     </div>
-                    <div className="position-relative">
-                        <div className="p-3" style={{overflowX: "auto",width:"90%"}}>
+                    <div className="position-relative" style={{overflowX: "auto"}}>
+                        <div className="p-3" style={{overflowX: "auto",width:"max-content"}}>
                             {
-                                this.state.naCodes.map((elem, index) => {
+                                this.state.naCodesShow.map((elem, index) => {
                                     return (
-                                        <div key={`modelCodeCosy-${elem.code}`} className="d-inline">
-                                            <input type="radio" class="btn-check" name="modelCodesSearch-option" id={`modelCodesSearch-option-${elem.code}`} autocomplete="off" />
-                                            <label className="btn btn-outline-primary text-center h-100" onClick={this.getPathFromNaCode} htmlFor={`modelCodesSearch-option-${elem.code}`} style={{width:"120px", height:"63px"}}>
-                                                <b>${elem.code}</b>
-                                                <small className="mb-0 d-block">${elem.name}</small>
+                                        <div key={`modelCodeCosy-${elem.code}`} className="d-inline-block me-2 position-relative">
+                                            {this.state.currentSelectedNaCode ? <button className="btn btn-primary no-shadow btn-xs position-absolute top-0 end-0" onClick={this.closeNACODEsearch} style={{width:"20px", height:"20px"}}> X </button> : null}
+                                            <input type="radio" className="btn-check" name="modelCodesSearch-option" id={`modelCodesSearch-option-${elem.code}`} data-na={elem.code} onChange={this.selectNACode} checked={this.state.currentSelectedNaCode ? true : false }/>
+                                            <label className="btn btn-outline-primary text-center h-100" htmlFor={`modelCodesSearch-option-${elem.code}`} style={{width:"120px",minHeight:'63px'}}>
+                                                <b>{elem.code}</b>
+                                                <small className="mb-0 d-block">{elem.name}</small>
                                             </label>
                                         </div>
                                     )
--- frontend/src/views/main/Settings.jsx
+++ frontend/src/views/main/Settings.jsx
@@ -34,8 +34,8 @@ export class Settings extends Component {
         try {
             const res = await axios.post(url, data);
             if (res.status === 200) {
-                sessionStorage.setItem('token_auth', email);
-                sessionStorage.setItem('user_auth', token);
+                localStorage.setItem('token_auth', email);
+                localStorage.setItem('user_auth', token);
             } else {
                 console.log('Error: ', res.status);
             }
@@ -54,15 +54,14 @@ export class Settings extends Component {
     }
     componentDidMount(){
         this.loadConfigDB();
-        this.loadPathsDB();
     }
     loadConfigDB = async () =>{
         const url = 'http://localhost:8895/api/v1/get/config';
         try {
             const res = await axios.get(url);
             if (res.status === 200) {
-                sessionStorage.setItem('email_auth', res.data.data.c_email);
-                sessionStorage.setItem('token_auth', res.data.data.c_token);
+                localStorage.setItem('email_auth', res.data.data.c_email);
+                localStorage.setItem('token_auth', res.data.data.c_token);
                 this.setState({
                     form:{
                         email: res.data.data.c_email,
@@ -71,30 +70,6 @@ export class Settings extends Component {
                     cosyYear: res.data.data.p_year,
                     favView: res.data.data.fav_view,
                 });
-            } else {
-                console.log('Error');
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    loadPathsDB = async () =>{
-        const url = 'http://localhost:8895/api/v1/get/configPaths';
-        try {
-            const res = await axios.get(url);
-            if (res.status === 200) {
-                this.setState({
-                    domain: res.data.data.domain,
-                    modelList: res.data.data.modelList,
-                    start: res.data.data.start,
-                    options: res.data.data.options,
-                    specs: res.data.data.specs,
-                });
-                sessionStorage.setItem('domain', res.data.data.domain);
-                sessionStorage.setItem('modelList', res.data.data.modelList);
-                sessionStorage.setItem('start', res.data.data.start);
-                sessionStorage.setItem('options', res.data.data.options);
-                sessionStorage.setItem('specs', res.data.data.specs);
             } else {
                 console.log('Error');
             }
@@ -142,33 +117,6 @@ export class Settings extends Component {
                         }
                     </select>
                 </div>
-                <div className="py-2">
-                    <hr/>
-                </div>
-                <h4 className="pb-2">CoSy</h4>
-                <div className="mb-3">
-                    <label htmlFor="cosyDomain" className="form-label text-light small mb-1">Domain</label>
-                    <input type="text" className="form-control bg-dark border-0" name="domain" onChange={this.handleChange} placeholder="Cosy domain..." value={this.state.domain}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="cosyModelList" className="form-label text-light small mb-1">Model Images List</label>
-                    <input type="text" className="form-control bg-dark border-0" name="modelList" onChange={this.handleChange} placeholder="Model Images List..." value={this.state.modelList}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="cosyStart" className="form-label text-light small mb-1">Image API</label>
-                    <input type="text" className="form-control bg-dark border-0" name="start" onChange={this.handleChange} placeholder="Image API..." value={this.state.start}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="cosyOptions" className="form-label text-light small mb-1">Image API Options</label>
-                    <input type="text" className="form-control bg-dark border-0" name="options" onChange={this.handleChange} placeholder="Image API Options..." value={this.state.options}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="techspecsApi" className="form-label text-light small mb-1">Techspecs API</label>
-                    <input type="text" className="form-control bg-dark border-0" name="specs" onChange={this.handleChange} placeholder="Techspecs API..." value={this.state.specs}/>
-                </div>
-                <button type="button" className="btn btn-primary"  id="validateButtonCosy">
-                    <i className="fas " id="validateButtonCosyIcon"></i><span>Save</span>
-                </button>
             </div>
         );
     }
