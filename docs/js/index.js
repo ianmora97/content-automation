fa26413084d280e139985eb511eda76877a1ebaa
Ian Mora <ianmorar03@gmail.com>
2022-03-14 21:53:46 -0600

    github page update

---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+function loaded(){
+    imageChange();
+}
+
+function imageChange(){
+    // change between images every 5 seconds
+    // var images = ["img/feature2.png", "img/feature3.png"];
+    // var i = 0;
+    // setInterval(function(){
+    //     document.getElementById("imageCosy").src = images[i];
+    //     i = (i + 1) % images.length;
+    // }, 5000);
+
+}
+
+document.addEventListener('DOMContentLoaded', loaded);
--- gulpfile.js
+++ gulpfile.js
@@ -34,18 +34,61 @@ gulp.task('js', function() {
         .pipe(concat('jq-bs-bundle.js'))
         .pipe(gulp.dest('../js'));
 });
-
 // watching all tasks
 gulp.task('serve', gulp.series('sass','js','animate','fontawesome', function() {
     gulp.watch("../sass/*.scss", gulp.series('sass'));
 }));
-
 // compile Fontawesome
 gulp.task('fonta', gulp.series('fontawesome'));
-
 // watching only scss task
 gulp.task('styles', gulp.series('sass', function() {
     gulp.watch("./public/sass/*.scss", gulp.series('sass'));
 }));
-
 gulp.task('default', gulp.series('styles'));
+
+// ! ------------------------------- For DOCS -----------------------------------------
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass-docs', function() {
+    return gulp.src("./docs/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./docs/css"));
+});
+
+// move animate.css to web/css
+gulp.task('animate-docs', function() {
+    return gulp.src('node_modules/animate.css/animate.css')
+        .pipe(concat('animate.css'))
+        .pipe(gulp.dest("./docs/css"));
+});
+
+// move fontawesome.css to web/css
+gulp.task('fontawesome-docs', function() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scss')
+        .pipe(sass())
+        .pipe(concat('fontawesome.css'))
+        .pipe(gulp.dest("./docs/css"));
+});
+
+// move bootstrap JS and Jquery
+gulp.task('js-docs', function() {
+    return gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+        ])
+        .pipe(concat('jq-bs-bundle.js'))
+        .pipe(gulp.dest('./docs/js'));
+});
+// watching all tasks
+gulp.task('serve-docs', gulp.series('sass-docs','js-docs','animate-docs','fontawesome-docs', function() {
+    gulp.watch("./docs/scss/*.scss", gulp.series('sass-docs'));
+}));
+
+// compile Fontawesome
+gulp.task('fonta-docs', gulp.series('fontawesome-docs'));
+
+// watching only scss task
+gulp.task('styles-docs', gulp.series('sass-docs', function() {
+    gulp.watch("./docs/scss/*.scss", gulp.series('sass-docs'));
+}));
+
+gulp.task('default-docs', gulp.series('styles-docs'));
